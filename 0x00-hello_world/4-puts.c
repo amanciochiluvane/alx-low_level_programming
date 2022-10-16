5b6abd8d3a4c4675070fdaaba18c4460f973a2d7
Your Name <you@example.com>
2022-10-16 10:24:38 -0700

    heyaah

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,19 +1,29 @@
 #include <stdio.h>
 
+#include <string.h>
+
+
+
 /**
+ *main - Main function of my project
  *
- * * main - Prints using puts() command
- *
- * * Return: 0 prints something
- *
- * */
+ * *Return: return 0 everytime
+ */
 
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
 
-			return (0);
+
+		char displayed[52];
+
+
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+
+				puts(displayed);
+
+					return (0);
 
 }
