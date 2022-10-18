793b50892d6175a40200e19187a391b9b81d959b
Your Name <you@example.com>
2022-10-18 11:48:46 -0700

    first one

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main- function that prints test
+ *
+ * Return:return 0
+ */
+
+int main(void)
+{
+	char putchar[10] = "_putchar";
+
+	int i;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		_putchar(putchar[i]);
+	}
+	_putchar(10);
+	return (0);
+}
+
