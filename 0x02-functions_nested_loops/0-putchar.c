934a593e4bbb841fca5cb1ac3cd632d6d43d719d
Your Name <you@example.com>
2022-10-18 12:31:09 -0700

    atualized

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	char putchar[10] = "_putchar";
+	char hello[10] = "_putchar";
 
 	int i;
 
 	for (i = 0 ; i < 8 ; i++)
 	{
-		_putchar(putchar[i]);
+		_putchar(hello[i]);
 	}
 	_putchar(10);
 	return (0);
