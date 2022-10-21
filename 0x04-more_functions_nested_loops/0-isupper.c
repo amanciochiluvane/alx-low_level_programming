e1a1a2f4f6246167fb5206642d4149c41beb9ec4
Your Name <you@example.com>
2022-10-21 13:24:43 -0700

    kjj

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -9,11 +9,8 @@
 
 int main(void)
 {
-	char c;
+if ((c >= 'A') && (c <= 'Z'))
+return (1);
 
-	c = 'A';
-	printf("%c: %d\n", c, _isupper(c));
-	c = 'a';
-	printf("%c: %d\n", c, _isupper(c));
-	return (0);
+return (0);
 }
