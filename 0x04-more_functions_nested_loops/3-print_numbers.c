2f29579e5f524b53540fb792086e772186847904
Your Name <you@example.com>
2022-10-22 01:32:20 -0700

    added

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that will print numbers
+ *
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+putchar(number);
+}
+putchar(10);
+}
+
