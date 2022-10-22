5b1b72b8c3379cbc14a1372662ddc40686aaae5a
Your Name <you@example.com>
2022-10-22 03:02:30 -0700

    added

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal- prints a diagonal
+ * @n: parameter
+ * Return: always nothing
+ */
+
+void print_diagonal(int n)
+{
+int len, space;
+
+if (n > 0)
+{
+for (len = 0; len < n; len++)
+{
+for (space = 0; space < len; space++)
+putchar(' ');
+
+putchar('\\');
+
+if (len == (n - 1))
+continue;
+putchar('\n');
+}
+}
+putchar('\n');
+}
