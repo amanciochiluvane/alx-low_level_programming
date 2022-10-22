0aa69af4b42661c533bcfc8a6e388c3f3b0e2e8c
Your Name <you@example.com>
2022-10-22 02:22:05 -0700

    added

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - prints a line
+ * @n:parameter
+ *Return: nothing
+ */
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+putchar('_');
+}
+putchar('\n');
+}
+
