b109ae3a8e9a405384679d47cbd957e3821e2049
Your Name <you@example.com>
2022-10-21 21:53:09 -0700

    add

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two members
+ * @a : number one
+ * @b : number two
+ * Return: rh  multiply of a and b
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
