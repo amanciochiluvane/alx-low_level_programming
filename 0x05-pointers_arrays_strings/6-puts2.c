4b5daf2a38a97a6cba8dce95092108f198dff42a
Your Name <you@example.com>
2022-10-24 11:56:41 -0700

    added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * puts2 - prints every other character
+ * @str: the string to use.
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+if (i % 2 == 0)
+{
+putchar(str[i]);
+}
+i++;
+}
+putchar('\n');
+}
