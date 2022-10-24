2b2d524f8465657461e80add77be16b60208bbd0
Your Name <you@example.com>
2022-10-24 11:12:42 -0700

    added

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return:  always 0
+ */
+
+int main(void)
+
+{
+
+
+ char *str;
+
+     int len;
+
+         str = "My first strlen!";
+
+	     len = _strlen(str);
+
+	         printf("%d\n", len);
+
+		     return (0);
+
+		     }
