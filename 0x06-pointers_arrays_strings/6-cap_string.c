3b366ee186f4e93f623b281cfbc9356575557054
Your Name <you@example.com>
2022-10-26 11:05:33 -0700

    added

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+
+
+/**
+ * cap_string - capitalizes all words of a string.
+ * @s: string.
+ * Return: s
+ */
+
+char *cap_string(char *s)
+{
+
+int i = 0;
+
+while (s[i] != '\0')
+{
+if ((s[i - 1] == ' ' || s[i - 1] == '\n'
+|| s[i - 1] == '\t' || s[i - 1] == ','
+|| s[i - 1] == ';' || s[i - 1] == '!'
+|| s[i - 1] == '?' || s[i - 1] == '"'
+|| s[i - 1] == '(' || s[i - 1] == ')'
+|| s[i - 1] == '{' || s[i - 1] == '}'
+|| s[i - 1] == '.')
+&& (s[i] >= 'a' && s[i] <= 'z'))
+{
+s[i] = s[i] - 32;
+}
+else if ((s[0] >= 97 && s[0] <= 122))
+{
+s[0] = s[0] - 32;
+}
+else
+{
+s[i] = s[i];
+}
+
+i++;
+
+}
+
+return (s);
+}
