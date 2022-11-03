252840ab720ea36418c1770097504767604d679d
Your Name <you@example.com>
2022-11-03 09:20:44 -0700

    added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -9,5 +9,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int _sqrt(int n, int x);
+int is_prime_number(int n);
+int is_prime(int n, int start);
 
 #endif
