c36ee96aa3df45f757d79583c43c6440c0047a0b
Your Name <you@example.com>
2022-11-03 09:56:48 -0700

    added

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -14,5 +14,7 @@ int is_prime(int n, int start);
 int is_palindrome(char *s);
 int find_strlen(char *s);
 int check_palindrome(char *s, int len, int index);
+int wildcmp(char *s1, char *s2);
+
 
 #endif
