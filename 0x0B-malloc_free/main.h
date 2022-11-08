e4313e62c7e622c84a04504bd15dd10478a75944
Your Name <you@example.com>
2022-11-08 11:20:50 -0800

    added

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -13,5 +13,7 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 char *argstostr(int ac, char **av);
 int len(char *str);
+char **strtow(char *str);
+int num_words(char *str);
 
 #endif
