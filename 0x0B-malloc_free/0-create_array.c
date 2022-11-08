3bffce5e7514a37ba6841215ad2ba7bd9b8536e3
Your Name <you@example.com>
2022-11-08 10:03:17 -0800

    added

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * create_array - creates an array
+ * @size: Size of the array
+ * @c: Character to insert
+ * Return: NULL or pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+char *array;
+unsigned int index;
+
+if (size == 0)
+	return (NULL);
+array = malloc(sizeof(char) * size);
+
+if (array == NULL)
+	return (NULL);
+
+for (index = 0; index < size; index++)
+	array[index] = c;
+
+return (array);
+}
+
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+
+
+void free_grid(int **grid, int height);
+char *create_array(unsigned int size, char c);
+
+#endif
