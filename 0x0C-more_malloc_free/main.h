35f1e8699d478523979d46ae2a621bd11d5bff0e
Your Name <you@example.com>
2022-11-10 12:05:01 -0800

    added

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,10 +3,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define min(x, y) (((x) < (y)) ? (x) : (y))
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
 #endif
