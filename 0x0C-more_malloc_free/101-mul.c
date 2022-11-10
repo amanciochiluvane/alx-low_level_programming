91a71d6ba422f681fba9108e57e80e69d08adfe8
Your Name <you@example.com>
2022-11-10 13:32:37 -0800

    added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-
-int find_len(char *str)
+/*
+* find_len -Finds the length
+*/
+int find_len(char *str);
 char *create_xarray(int size);
 char *iterate_zeroes(char *str);
 void get_prod(char *prod, char *mult, int digit, int zeroes);
 void add_nums(char *final_prod, char *next_prod, int next_len);
-
 
 /**
  * find_len - Finds the length of a string.
@@ -204,3 +205,5 @@ free(final_prod);
 
 return (0);
 }
+
+
