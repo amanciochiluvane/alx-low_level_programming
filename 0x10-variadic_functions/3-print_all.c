966c20f7abb7e285a30c614a128a6091931bc831
Your Name <you@example.com>
2022-11-17 12:31:43 -0800

    added

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,7 +2,7 @@
 
 /**
  * print_char - Prints a char.
- * @arg: A list of arguments 
+ * @arg: A list of arguments
  */
 void print_char(va_list arg)
 {
@@ -30,6 +30,8 @@ printf("%d", num);
  *print_float - Prints a float.
  *@arg: A list of arguments
  */
+void print_float(va_list arg)
+{
 float num;
 
 num = va_arg(arg, double);
@@ -37,3 +39,62 @@ num = va_arg(arg, double);
 printf("%f", num);
 }
 
+/**
+ * print_string - Prints a string.
+ * @arg: A list of arguments
+ */
+void print_string(va_list arg)
+{
+char *str;
+
+str = va_arg(arg, char *);
+
+if (str == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", str);
+}
+
+/**
+ * print_all - prints anything
+ * @format: format of input
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+va_list args;
+
+int i = 0, j = 0;
+
+char *separator = "";
+
+printer_t funcs[] = {
+{"c", print_char},
+{"i", print_int},
+{"f", print_float},
+{"s", print_string}
+};
+
+va_start(args, format);
+
+while (format && (*(format + i)))
+{
+j = 0;
+
+while (j < 4 && (*(format + i) != *(funcs[j].symbol)))
+j++;
+
+if (j < 4)
+{
+printf("%s", separator);
+funcs[j].print(args);
+separator = ", ";
+}
+i++;
+}
+printf("\n");
+
+va_end(args);
+}
